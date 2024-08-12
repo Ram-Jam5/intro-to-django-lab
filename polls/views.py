efe80afd0ff9366543a2b9f819a2a1404188aa98
Ram-Jam5 <edgeillusive@gmail.com>
2024-08-12 11:01:32 -0500

    exercise completed

--- polls/views.py
+++ polls/views.py
@@ -31,7 +31,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+        
         return render(
             request,
             "polls/detail.html",
@@ -43,7 +43,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
